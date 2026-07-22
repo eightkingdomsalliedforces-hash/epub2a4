97280fd5472910cf52dfc5ae7d2fbcce9a21332e
eightkingdomsalliedforces-hash <eightkingdomsalliedforces@gmail.com>
2026-07-22 19:02:35 +0800

    feat: add Compose theme

---
+++ app/src/main/java/tw/daniel/epubword/ui/theme/Theme.kt
@@ -0,0 +1,19 @@
+package tw.daniel.epubword.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EpubWordTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    MaterialTheme(
+        colorScheme = if (darkTheme) darkColorScheme() else lightColorScheme(),
+        typography = MaterialTheme.typography,
+        content = content,
+    )
+}
