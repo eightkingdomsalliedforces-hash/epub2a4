3527cfe2c532cc599c8fa6001fc6f98f73996148
eightkingdomsalliedforces-hash <eightkingdomsalliedforces@gmail.com>
2026-07-24 23:32:56 +0800

    refactor: remove obsolete Android Python package copy

--- app/src/main/python/epub_a4_word/__init__.py
+++
@@ -1,8 +0,0 @@
-"""EPUB imposition and DOCX reflow converter."""
-
-from .converter import convert_input
-from .epub import EpubError, parse_epub
-from .word_reflow import convert_docx
-
-__version__ = "0.5.0"
-__all__ = ["EpubError", "parse_epub", "convert_input", "convert_docx", "__version__"]
