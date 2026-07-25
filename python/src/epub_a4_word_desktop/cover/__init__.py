e19ed6a74f1e647ebabbe2288d0ad88373ea54ca
eightkingdomsalliedforces-hash <eightkingdomsalliedforces@gmail.com>
2026-07-25 11:30:58 +0800

    feat: export desktop cover canvas API

--- python/src/epub_a4_word_desktop/cover/__init__.py
+++ python/src/epub_a4_word_desktop/cover/__init__.py
@@ -1,10 +1,18 @@
+from .canvas import CoverCanvas, ZoomControls
 from .commands import ReplaceProjectCommand
 from .controller import CoverController, PreviewWorker
+from .guides import GuideLayer
+from .items import CoverImageItem, CoverTextItem
 from .models import patch_element
 
 __all__ = [
+    "CoverCanvas",
     "CoverController",
+    "CoverImageItem",
+    "CoverTextItem",
+    "GuideLayer",
     "PreviewWorker",
     "ReplaceProjectCommand",
+    "ZoomControls",
     "patch_element",
 ]
