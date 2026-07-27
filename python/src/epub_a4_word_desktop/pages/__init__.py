e75747568cd7de9abd333f8689fa70ac5e17ff93
eightkingdomsalliedforces-hash <eightkingdomsalliedforces@gmail.com>
2026-07-27 19:31:24 +0800

    refactor: remove temporary publisher prompt hook

--- python/src/epub_a4_word_desktop/pages/__init__.py
+++ python/src/epub_a4_word_desktop/pages/__init__.py
@@ -1,8 +1,5 @@
 from .converter_page import ConverterPage
 from .cover_page import CoverPage
 from .home_page import HomePage
-from .publisher_change import install_publisher_change_prompt
-
-install_publisher_change_prompt(CoverPage)
 
 __all__ = ["CoverPage", "ConverterPage", "HomePage"]
