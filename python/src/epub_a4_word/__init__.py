f87099feb22e9b8a8afbd1ae2d067a97611fc9ad
eightkingdomsalliedforces-hash <eightkingdomsalliedforces@gmail.com>
2026-07-28 00:10:42 +0800

    chore: prepare v0.6.3

--- python/src/epub_a4_word/__init__.py
+++ python/src/epub_a4_word/__init__.py
@@ -4,5 +4,5 @@ from .converter import convert_input
 from .epub import EpubError, parse_epub
 from .word_reflow import convert_docx
 
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 __all__ = ["EpubError", "parse_epub", "convert_input", "convert_docx", "__version__"]
