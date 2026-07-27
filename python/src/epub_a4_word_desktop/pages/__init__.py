43c61ba16bc2983dba1776bc3378ea270122c499
eightkingdomsalliedforces-hash <eightkingdomsalliedforces@gmail.com>
2026-07-27 19:24:38 +0800

    feat: prompt when publisher changes

--- python/src/epub_a4_word_desktop/pages/__init__.py
+++ python/src/epub_a4_word_desktop/pages/__init__.py
@@ -1,5 +1,8 @@
 from .converter_page import ConverterPage
 from .cover_page import CoverPage
 from .home_page import HomePage
+from .publisher_change import install_publisher_change_prompt
+
+install_publisher_change_prompt(CoverPage)
 
 __all__ = ["CoverPage", "ConverterPage", "HomePage"]
